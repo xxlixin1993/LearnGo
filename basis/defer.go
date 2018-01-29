4411a2334fece6fd540f61227bb83238383c3ed6
xxlixin1993 <403136170@qq.com>
2018-01-29 15:26:59 +0800

    学习defer

---
+++ basis/defer.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"time"
+	"log"
+)
+
+func main() {
+	bigSlowOperation()
+}
+
+func bigSlowOperation() {
+	defer trace("bigSlowOperation")() // don't forget the extra parentheses
+	// ...lots of work…
+	time.Sleep(10 * time.Second) // simulate slow operation by sleeping
+}
+func trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() {
+		log.Printf("exit %s (%s)", msg,time.Since(start))
+	}
+}
